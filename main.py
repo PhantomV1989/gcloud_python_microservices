b99fcdb0574070684c2de02d40630897675e28e0
tan guan hong <tanguanhong89@gmail.com>
2019-01-24 17:56:57 +0800

    working v1

--- main.py
+++ main.py
@@ -1,51 +1,68 @@
-#!/usr/bin/env python
-
-# Copyright 2016 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""Simple application that performs a query with BigQuery."""
-# [START bigquery_simple_app_all]
-# [START bigquery_simple_app_deps]
-from google.cloud import bigquery
+from flask import Flask, jsonify, request  # simple server
+import datetime
 import os
 
-
-def query_stackoverflow():
-    # [START bigquery_simple_app_client]
-    client = bigquery.Client()
-    # [END bigquery_simple_app_client]
-    # [START bigquery_simple_app_query]
-    query_job = client.query("""
-        SELECT
-          CONCAT(
-            'https://stackoverflow.com/questions/',
-            CAST(id as STRING)) as url,
-          view_count
-        FROM `bigquery-public-data.stackoverflow.posts_questions`
-        WHERE tags like '%google-bigquery%'
-        ORDER BY view_count DESC
-        LIMIT 10""")
-
-    results = query_job.result()  # Waits for job to complete.
-    # [END bigquery_simple_app_query]
-
-    # [START bigquery_simple_app_print]
-    for row in results:
-        print("{} : {} views".format(row.url, row.view_count))
-    # [END bigquery_simple_app_print]
+app = Flask(__name__)
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/phantomv/Downloads/sephora-interview-229603-c9967bbe7ccd.json"
 
 
-if __name__ == '__main__':
-    query_stackoverflow()
-# [END bigquery_simple_app_all]
+@app.route('/')
+def default():
+    with open('front.html', 'rb') as f:
+        html_content = f.read()
+    return html_content
+
+
+@app.route('/css')
+def css():
+    with open('css.css', 'rb') as f:
+        html_content = f.read()
+    return html_content
+
+
+@app.route('/query', methods=['GET'])
+def query():
+    try:
+        content = request.values
+        for k in content:
+            print(k, ':', content[k])
+
+        from google.cloud import bigquery
+
+        client = bigquery.Client()
+        _h = lambda x: x + " like '%" + content[x] + "%'  " if content[x] != '' else ''
+        _add_day = lambda x: (datetime.datetime.strptime(x, '%Y-%m-%d') + datetime.timedelta(days=1)).strftime(
+            '%Y-%m-%d')
+        title = _h('title')
+        text = _h('text')
+        time = "time_ts<'{}' and time_ts>='{}'".format(_add_day(content['date']), content['date']) if \
+            content['date'] != '' else ''
+
+        where = '' if title != '' and text != '' and time != '' else 'where '
+        arg = ' and '.join(list(filter(lambda x: x != '', [title, text, time])))
+
+        query_s = """
+            SELECT title,url,text,time_ts
+            FROM `bigquery-public-data.hacker_news.stories`
+            {}{}
+            limit 10
+        """.format(where, arg)
+
+        query_job = client.query(query_s)
+
+        results = query_job.result()  # Waits for job to complete.
+
+        post_resp = {}
+        for i, v in enumerate(results):
+            ele = {}
+            for k in list(v.keys()):
+                ele[k] = v[k]
+            post_resp[i] = ele
+        return jsonify(post_resp)
+    except Exception as e:
+        print(str(e))
+        return str(e)
+
+
+if __name__ == "__main__":
+    app.run(host=app.config.get("HOST", "localhost"), port=app.config.get("PORT", 8000))
